89d5cc48e3506996ec11919bd5e6327be6f5395e
Dani0000 <daniel.staikov@gmail.com>
2017-06-18 21:23:01 +0300

    upload files

---
+++ ConsoleInOut/ConsoleInOut.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleInOut
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string name = Console.ReadLine(); //Чете от конзолата. Очаква текст.
+            
+            Console.WriteLine("Hello "+name);
+
+            char z = 'z';
+            Console.WriteLine((int)z);
+            Console.WriteLine(Convert.ToString(z));// нещо изтървах. трябва да изкара двоичен код на символа "z".
+          }
+    }
+}
---
+++ MultiplyNumbers/MultiplyNumbers.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MultiplyNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int number1 = 10;
+            int number2 = 10;
+            int result = number1 * number2;
+            Console.WriteLine("Multiplied = " + result); // няколко начина на изписване на оператора
+            Console.WriteLine($"Multiplied = {result}");
+            Console.WriteLine("Multiplied = {0}",result); // след "Multiplied" със запетая, може да има изброени много променливи и в скобите указваш, коя променлива да изкара на екрана.
+        }
+    }
+}
